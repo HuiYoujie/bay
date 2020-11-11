53938a755915eb16e5507ba4e46b0caab76101e5
HuiYoujie <42954611+HuiYoujie@users.noreply.github.com>
2020-11-11 22:45:20 +0800

    页面架构整理

---
+++ pages/mine/inedx/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <view class="content">
+		<!-- 个人信息 -->
+
+		<!-- 工具列表 -->
+        <u-cell-group title="tools">
+            <u-cell-item title="计算日期" label="两种计算方式"></u-cell-item>
+            <u-cell-item title="备忘录"></u-cell-item>
+        </u-cell-group>
+
+		<!-- 设置 -->
+        <u-cell-group title="settings">
+            <u-cell-item title="主题"></u-cell-item>
+            <u-cell-item title="提示开关" arrow="false">
+				<u-switch slot="right-icon" v-model="remind_checked" size="40"></u-switch>
+			</u-cell-item>
+        </u-cell-group>
+    </view>
+</template>
+
+<script>
+import log from '../../../utils/log';
+import config from '../../../config.js';
+const globalData = getApp().globalData;
+export default {
+    data() {
+        return {
+			remind_checked: true,
+        };
+    },
+    onLoad() {},
+    methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.content {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
---
+++ pages/thing/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<u-search v-model="value" @change="change" @custom="custom" @search="search" :shape="shape" :clearabled="clearabled" :show-action="showAction" :input-align="inputAlign" @clear="clear"></u-search>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		text-align: center;
+		height: 400upx;
+		margin-top: 200upx;
+	}
+</style>
