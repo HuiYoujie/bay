1674c7a11a6288ea5bcabeb44b4af25cce308443
HuiYoujie <42954611+HuiYoujie@users.noreply.github.com>
2020-12-14 23:46:42 +0800

    云函数1

--- static/cloudfunctions/login/index.js
+++ static/cloudfunctions/login/index.js
@@ -2,6 +2,7 @@
 // 部署：在 cloud-functions/login 文件夹右击选择 “上传并部署”
 
 const cloud = require('wx-server-sdk')
+// const defaultRoomList = require('./roomList.js')
 
 // 初始化 cloud
 cloud.init({
@@ -48,60 +49,69 @@ exports.main = async (event, context) => {
 	const user = await db.collection('user').where({
 		_openid
 	}).get()
-	if (user.data.length <= 0) {
-		let newUser = {
-			_openid,
-			userId: 1,
-			createdTime: db.serverDate(),
-			updateTime: db.serverDate(),
-			status: 0,
-			phone: '',
-			gender: null,
-			nickName: '',
-			avatar: '',
-			country: '',
-			province: '',
-			city: ''
-		}
-		// 添加新的用户
-		db.collection('user').add({
-			data: newUser
-		}).then(res => {
-			console.log(res)
-		}).catch(err => {
-			console.log(err)
-		})
-	} else {
-		console.log('获取到当前的登录用户', resp.data[0])
-		let newUser = resp.data[0]
-		newUser.updateTime = db.serverDate()
-		let _id = newUser._id
-		delete newUser._id
-		// 更新用户信息
-		db.collection('user').doc(_id).update({
-			data: newUser
-		}).then(resp => {
-			// console.log(resp)
-		}).catch(err => {
-			console.log(err)
-		})
-	}
+	// 查询user表数据条数
+	const userNumber = await db.collection('user').count()
+	console.log(userNumber)
+	// if (user.data.length <= 0) {
+	// 	// 查询user表数据条数
+	// 	const userNumber = await db.collection('user').count()
+	// 	let newUser = {
+	// 		_openid,
+	// 		userId: Number(userNumber) + 1,
+	// 		status: 0,
+	// 		phone: '',
+	// 		gender: null,
+	// 		nickName: '',
+	// 		avatar: '',
+	// 		country: '',
+	// 		province: '',
+	// 		city: '',
+	// 		createdTime: db.serverDate(),
+	// 		updateTime: db.serverDate()
+	// 	}
+	// 	// 添加新的用户
+	// 	db.collection('user').add({
+	// 		data: newUser
+	// 	}).then(res => {
+	// 		console.log(res)
+	// 	}).catch(err => {
+	// 		console.log(err)
+	// 	})
+	// } else {
+	// 	console.log('获取到当前的登录用户', resp.data[0])
+	// 	let user = resp.data[0]
+	// 	user.updateTime = db.serverDate()
+	// 	// let _id = user._id
+	// 	// delete user._id
+	// 	// 更新用户信息
+	// 	db.collection('user').doc(_id).update({
+	// 		data: user
+	// 	}).then(resp => {
+	// 		console.log(resp)
+	// 	}).catch(err => {
+	// 		console.log(err)
+	// 	})
+	// }
 	
-	// 登录后，自动创建默认家庭，允许修改名称，但不可以删除
+	// // 查询family表数据条数
+	// const familyNumber = await db.collection('family').count()
+	// // 登录后，自动创建默认家庭，允许修改名称，但不可以删除
 	// db.collection('family').where({
 	// 	_openid
 	// }).get().then(resp => {
-	// 	// 不存在默认账本，创新新的默认账本
+	// 	// 不存在默认家庭，创新新的默认家庭
 	// 	if (resp.data.length <= 0) {
 	// 		let family = {
-	// 			"status": 0,
-	// 			"createdTime": new Date(),
-	// 			"updateTime": new Date(),
-	// 			"user": wxContext.OPENID,
-	// 			"name": '默认账本',
-	// 			"isDefault": true,
-	// 			"isAuto": true,
-	// 			"_openid": wxContext.OPENID
+	// 			_openid,
+	// 			user: wxContext.OPENID,
+	// 			userId: Number(userNumber) + 1,
+	// 			familyId: familyNumber + 1,
+	// 			name: '默认家庭',
+	// 			status: 0,
+	// 			isDefault: true,
+	// 			isAuto: true,
+	// 			createdTime: new Date(),
+	// 			updateTime: new Date(),
 	// 		}
 
 	// 		// 新建家庭 默认一个
@@ -114,13 +124,22 @@ exports.main = async (event, context) => {
 	// 		})
 			
 	// 		// 新建房间 默认8个 => 主卧 次卧 客厅 厨房 餐厅 卫生间 玄关 阳台
-	// 		db.collection('room').add({
-	// 			data: book
-	// 		}).then(resp => {
-	// 			console.log(resp)
-	// 		}).catch(err => {
-	// 			console.log(err)
-	// 		})
+	// 		for(data of defaultRoomList) {
+	// 			// 查询room表数据条数
+	// 			const roomNumber = await db.collection('room').count()
+	// 			data.roomId = roomNumber + 1
+	// 			data._openid,
+	// 			data.userId: Number(userNumber) + 1,
+	// 			data.familyId: Number(familyNumber) + 1,
+	// 			data.roomId: roomNumber + 1,
+	// 			db.collection('room').add({
+	// 				data
+	// 			}).then(resp => {
+	// 				console.log(resp)
+	// 			}).catch(err => {
+	// 				console.log(err)
+	// 			})
+	// 		}
 	// 	}
 	// })
 	
---
+++ static/cloudfunctions/login/roomList.js
@@ -0,0 +1,44 @@
+exports.defaultRoomList = [
+	{
+		_openid,
+		userId: Number(userNumber) + 1,
+		familyId: familyNumber + 1,
+		roomId: 0,
+		name: '卧室',
+		status: 0,
+		isDefault: true,
+		isAuto: true,
+		createdTime: new Date(),
+		updateTime: new Date(),
+	}, {
+		name: '次卧',
+		status: 0,
+		isDefault: true,
+		isAuto: true,
+		createdTime: new Date(),
+		updateTime: new Date(),
+	}, {
+		_openid,
+		userId: Number(userNumber) + 1,
+		familyId: familyNumber + 1,
+		roomId: 0,
+		name: '次卧',
+		status: 0,
+		isDefault: true,
+		isAuto: true,
+		createdTime: new Date(),
+		updateTime: new Date(),
+	}, {
+		_openid,
+		userId: Number(userNumber) + 1,
+		familyId: familyNumber + 1,
+		name: '次卧',
+		roomId: 0,
+		status: 0,
+		isDefault: true,
+		isAuto: true,
+		createdTime: new Date(),
+		updateTime: new Date(),
+	}
+]
+			
